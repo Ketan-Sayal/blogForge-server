9e0d80e2a9146a99f0912ab47ce6aef9dcd897a9
username <156992063+Ketandetrfyd13rd3ydf7h@users.noreply.github.com>
2026-01-18 14:17:12 +0530

    corrected path

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -1,8 +1,11 @@
 import multer from "multer";
+import path from "path";
+
+const uploadPath = path.join(process.cwd(), "public", "temp");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './public/temp')
+    cb(null, uploadPath)
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
